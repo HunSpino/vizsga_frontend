99cb945eab5c93f5c7d06f083b94f0610e19daad
HunSpino <tamasleventelaszlo99@gmail.com>
2022-04-05 16:43:15 +0200

    lel

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+  <div id="app" class="container">
+    <Header>
+      <h1>Vizsga</h1>
+      <nav>
+        <ul class="nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Táblázat</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Form</a>
+          </li>
+        </ul>
+      </nav>
+    </Header>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <VizsgaItem />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import VizsgaItem from './components/VizsgaItem.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    VizsgaItem
   }
 }
 </script>
