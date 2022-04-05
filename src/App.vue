bb158591501f91fc825b2ca87ff1b79d917b4e83
HunSpino <tamasleventelaszlo99@gmail.com>
2022-04-05 17:45:11 +0200

    végre van 404

--- src/App.vue
+++ src/App.vue
@@ -43,7 +43,7 @@ export default {
     },
     methods: {
       async loadData () {
-      let Response = await fetch('localhost/phpmyadmin/vizsga')
+      let Response = await fetch('http://localhost/vizsga')
       let data = await Response.json()
       this.vizsgas = data
     }
